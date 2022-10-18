55cb6840a16fc480073d5922fccc5ea8e31b56d5
satokadumasa <sato.kadumasa@gmail.com>
2022-10-18 22:36:25 +0900

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <div>{{message}}</div>
+        <RouterLink to="/inspire">トップページへ</RouterLink>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'Hello NextPage!'
+    }
+  }
+}
+</script>
