2f9237e4802f5674a002d0d2ebf5c2bb2b99355c
satokadumasa <sato.kadumasa@gmail.com>
2023-08-24 10:23:29 +0900

    feature/websocket

--- api-app/app/channels/chat_channel.rb
+++ api-app/app/channels/chat_channel.rb
@@ -4,7 +4,7 @@ class ChatChannel < ApplicationCable::Channel
     stream_from "chat_#{params[:room]}"
     nowis = DateTime.now.strftime("%Y年%m月%d日 %H時%M分%S秒")
     user = User.find(params[:user_id])
-    ActionCable.server.broadcast("chat_#{params[:room]}", { sender: user[:nickname],user_id: "1", username: user[:nickname] , message: "CONNECTED", now: nowis })
+    ActionCable.server.broadcast("chat_#{params[:room]}", { sender: user[:nickname],user_id: "1", username: user[:nickname] , message: "[" << user[:nickname] << "] JOIN THIS ROOM", now: nowis })
   end
 
   def unsubscribed
@@ -15,7 +15,7 @@ class ChatChannel < ApplicationCable::Channel
   def speak(data)
     pp "ChatChannel::speak() START"
     nowis = DateTime.now.strftime("%Y年%m月%d日 %H時%M分%S秒")
-    ActionCable.server.broadcast("chat_#{params[:room]}", { sender: data["sender"], user_id: data["user_id"], username: data["username"] , message: data["message"], now: nowis})
+    ActionCable.server.broadcast("chat_#{params[:room]}", { sender: data["username"], user_id: data["user_id"], username: data["username"] , message: data["message"], now: nowis})
     pp "ChatChannel::speak() END"
   end
 end
