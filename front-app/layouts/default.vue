97cf97a92c29b78113d2f244fd0386abcb34d738
satokadumasa <sato.kadumasa@gmail.com>
2024-01-04 00:59:30 +0900

    Front のデフォルトレイアウトを修正

--- front-app/layouts/default.vue
+++ front-app/layouts/default.vue
@@ -29,27 +29,21 @@
       fixed
       app
     >
-    <br>
-    <br>
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-    <v-toolbar-title>{{ title }}</v-toolbar-title>
-    <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-      </v-app-bar>
-      <v-main>
-        <v-container>
-          <Nuxt />
-        </v-container>
-      </v-main>
-      <v-footer
-        :absolute="fixed"
-        app
-      >
+      <br>
+      <br>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="fixed"
+      app
+    >
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
